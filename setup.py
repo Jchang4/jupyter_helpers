a4757a593f8b9f902c152f42341a40c4430ecde0
Justin Chang <justinchangmusic@gmail.com>
2022-12-22 20:17:21 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyter_helpers",
+    version="0.0.1",
+    url="https://github.com/Jchang4/jupyter_helpers.git",
+    author="Justin Chang",
+    author_email="",
+    description="",
+    install_requires=["jupyter", "fastai", "torch"],
+    packages=setuptools.find_packages(),
+)
